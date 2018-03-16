ce8f579eeffa0c38b80eed914c8751d4883fb557
Hunter Betz <hhb311@gmail.com>
2018-03-15 21:35:57 -0400

    Working on data preprocessing

--- brooklyn_sales/brooklyn_sales.py
+++ brooklyn_sales/brooklyn_sales.py
@@ -1,5 +1,7 @@
 __author__ = 'Hunter'
 from db import check_table_exists
+import numpy as np
+import pandas as pd
 import csv
 import json
 import psycopg2
@@ -7,7 +9,7 @@ import psycopg2
 # Create our table in the database
 def create_table(dbconn, tablename, header):
 	cur = dbconn.cursor()
-	columns = map(lambda c: '%s text' % c, header)
+	columns = ','.join(['%s text' % c for c in header])
 	queries = [
 		'DROP TABLE %s;' % tablename,
 		'CREATE TABLE %s(%s);' % (tablename, columns),
@@ -17,21 +19,32 @@ def create_table(dbconn, tablename, header):
 	for query in queries:
 		cur.execute(query)
 
+def insert_data(dbconn, tablename, columns, data):
+	cur = dbconn.cursor()
+	column_str = ','.join(columns)
+	data_str = ','.join(data)
+	query = 'INSERT INTO %s (%s) VALUES (%s)' % (tablename, column_str, data_str)
+	dbconn.commit()
 
 if __name__ == "__main__":
+	# TODO Fill in empty columns
+	# data = np.genfromtxt('brooklyn_sales.csv', delimiter=',')
+	data = pd.read_csv('brooklyn_sales.csv')
+
 	# Load JSON info for db connection
 	db_file = open('db_info.json', 'r')
 	db_json = json.load(db_file)
-	conn_str = 'host=%s dbname=%s user=%s password=%s port=%' % (db_json.get('host'), db_json.get('dbname'), db_json.get('user'), db_json.get('password'), db_json.get('port'))
+	conn_str = "host='%s' dbname='%s' user='%s' password='%s' port='%s'" % (db_json.get('host'), db_json.get('dbname'), db_json.get('user'), db_json.get('password'), db_json.get('port'))
 	db_file.close()
 
 	# Load data from csv file to input into database
-	csv_file = open('brooklyn_sales_map.csv', 'r')
+	csv_file = open('brooklyn_sales.csv', 'r')
 	reader = csv.reader(csv_file)
-	columns = next(reader)[1:]
+	columns = next(reader)
 
 	conn = psycopg2.connect(conn_str)
-	if not check_table_exists(conn, 'brooklyn_sales'):
-		create_table(conn, 'brooklyn_sales')
+	create_table(conn, 'brooklyn_sales', columns)
+	for row in reader:
+		insert_data(conn, 'brooklyn_sales', columns, row)
 
 	csv_file.close()
--- db.py
+++ db.py
@@ -2,11 +2,8 @@ __author__ = 'Hunter'
 
 def check_table_exists(dbconn, tablename):
 	dbcur = dbconn.cursor()
-	dbcur.execute("""
-		SELECT COUNT(*)
-		FROM Kaggle.tables
-		WHERE table_name = '{0}'
-		""".format(tablename.replace('\'', '\'\'')))
+	dbcur.execute("SELECT COUNT(*) FROM Kaggle.tables WHERE table_name = %s", ('%s' % tablename,))
+	dbcur.execute("""SELECT table_name FROM kaggle.tables WHERE table_schema = 'public'""")
 	if dbcur.fetchone()[0] == 1:
 		dbcur.close()
 		return True
