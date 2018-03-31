8d9d30b8efb7dc4257f03f1bda17a83a5482d370
Hunter Betz <hhb311@gmail.com>
2018-03-31 18:14:38 -0400

    Working on data preprocessing

--- brooklyn_sales/brooklyn_sales.py
+++ brooklyn_sales/brooklyn_sales.py
@@ -1,10 +1,10 @@
 __author__ = 'Hunter'
 import numpy as np
-import pandas as pd
 import re
 import csv
 import json
 import psycopg2
+
 
 # Check if table exists
 def check_table_exists(dbconn, tablename):
@@ -26,6 +26,7 @@ def create_table(dbconn, tablename, header):
 		cur.execute(query)
 	dbconn.commit()
 
+# Formats the string so it can be used in the query
 def format_record(record):
 	record = [re.sub('\s+', ' ', r).strip().replace("'", '') for r in record]
 	return record
@@ -35,15 +36,31 @@ def insert_data(dbconn, tablename, columns, data):
 	cur = dbconn.cursor()
 	column_str = ','.join(columns)
 	record = tuple(format_record(data))
-	# data_str = ','.join(record)
 	query = 'INSERT INTO %s (%s) VALUES %s;' % (tablename, column_str, record)
-	try:
-		cur.execute(query)
-	except:
-		print('a')
+	cur.execute(query)
+
+# Calculates median and inserts these values into the missing values of our data
+def preprocess_data(data):
+	temp_dat = []
+	for dat in data:
+		if dat > 0:
+			temp_dat.append(dat)
+	temp_dat = np.array(temp_dat)
+	median = np.median(temp_dat)
+	for idx, dat in enumerate(data):
+		if dat == 0:
+			data[idx] = median
+	return data
+
+# Performs gradient descent
+def gradient_descent_multi(x, y, theta, alpha, iterations):
+	m = len(y)
+
+	for j in range(iterations):
+		h = x.dot(theta)
+		delta = (1 / m) * np.sum(np.multiply(x, (h - y)))
 
 if __name__ == "__main__":
-	# TODO Fill in empty columns
 	# Load JSON info for db connection
 	print('Connecting to database')
 	db_file = open('db_info.json', 'r')
@@ -54,12 +71,11 @@ if __name__ == "__main__":
 	conn.autocommit = True
 	print('Successfully connected to %s' % db_json.get('dbname'))
 
+	csv_file = open('brooklyn_sales.csv', 'r')
+	reader = csv.reader(csv_file)
+	columns = next(reader)
 	if not check_table_exists(conn, 'brooklyn_sales'):
 		# Load data from csv file to input into database
-		csv_file = open('brooklyn_sales.csv', 'r')
-		reader = csv.reader(csv_file)
-		columns = next(reader)
-
 		print('Creating table %s' % 'brooklyn_sales')
 		create_table(conn, 'brooklyn_sales', columns)
 		print('Table created.')
@@ -73,7 +89,20 @@ if __name__ == "__main__":
 		csv_file.close()
 	else:
 		print('Data already loaded into database.')
-	# TODO start predicting!
-	query = 'SELECT * FROM brooklyn_sales ORDER BY id DESC LIMIT 10'
+
+	# Get data from db
 	cur = conn.cursor()
+	query = 'SELECT gross_sqft, sale_price FROM brooklyn_sales LIMIT 10000;'
 	cur.execute(query)
+	fetch = cur.fetchall()
+	sqft = []
+	price = []
+	for tup in fetch:
+		sqft.append(int(float(tup[0])))
+		price.append(int(float(tup[1])))
+	sqft = preprocess_data(np.array(sqft))
+	price = preprocess_data(np.array(price))
+	theta = np.zeros((1,1))
+	alpha = 0.01
+	iterations = 400
+	gradient_descent_multi(sqft, price, theta, alpha, iterations)
